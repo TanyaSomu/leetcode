48523650a2a851a807d9bad13d96846b1e116275
TanyaSomu <tanyasomu6@gmail.com>
2026-06-24 10:34:23 +0530

    Time: 33 ms (34.46%), Space: 108.4 MB (43.86%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<tasks.length;i++){
+            mp.put(tasks[i],mp.getOrDefault(tasks[i],0)+1);
+        } 
+        int res=0;
+        for(int num:mp.values()){
+            if(num==1) return -1;
+            // res+=num/3;
+            res+=(num+2)/3;
+            // if(num%3!=0) res++;
+        }
+        return res;
+    }
+}
