93ff1fb6ce5f9721c63214d9e0572a15fc5048ea
TanyaSomu <tanyasomu6@gmail.com>
2026-06-25 14:09:25 +0530

    Time: 76 ms (70.81%), Space: 98.2 MB (56.82%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -7,13 +7,25 @@ class Solution {
             cars[i][1]=speed[i];
         }
         Arrays.sort(cars,(a,b)->b[0]-a[0]);
-        Stack<Double> st=new Stack<>();
-        for(int[] car:cars){
-            double time=(double)(target-car[0])/car[1];
-            if(st.isEmpty()|| time>st.peek()){
-                st.push(time);
+        // Stack<Double> st=new Stack<>();
+        // for(int[] car:cars){
+        //     double time=(double)(target-car[0])/car[1];
+        //     if(st.isEmpty()|| time>st.peek()){
+        //         st.push(time);
+        //     }
+        // }
+        // return st.size();
+        double max=Integer.MIN_VALUE;
+        int count=0;
+        for(int i=0;i<n;i++){
+            int pos=cars[i][0];
+            int spd=cars[i][1];
+            double time=(double) (target-pos)/spd;
+            if(time>max){
+                count++;
+                max=time;
             }
         }
-        return st.size();
+        return count;
     }
 }
