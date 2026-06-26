dad9e31f32d9d958ace930115c1ab787bf36e56f
TanyaSomu <tanyasomu6@gmail.com>
2026-06-26 11:28:58 +0530

    Time: 2 ms (100%), Space: 199.1 MB (64.54%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int ans=0;
+        for(int i=0;i<derived.length;i++){
+            ans^=derived[i];
+        }
+        if(ans==0) return true;
+        return false;
+    }
+}
