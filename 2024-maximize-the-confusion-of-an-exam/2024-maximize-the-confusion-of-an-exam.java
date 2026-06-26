de568a324f5e5c7459f75a3c69f04a11ee42334c
TanyaSomu <tanyasomu6@gmail.com>
2026-06-26 15:29:30 +0530

    Time: 9 ms (99.31%), Space: 46.1 MB (92.75%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int l=0;
+        int maxF=0;
+        int result=0;
+        int n=answerKey.length();
+        int[] count=new int[26];
+        for(int r=0;r<n;r++){
+            maxF=Math.max(maxF,++count[answerKey.charAt(r)-'A']);
+            while(r-l+1>maxF+k){
+                --count[answerKey.charAt(l++)-'A'];
+            }
+            result=Math.max(result,r-l+1);
+        }
+        return result;
+    }
+}
