00051d9f5fd254df82401c711d2f2209b94c37c1
Tanya Somu <tanyasomu6@gmail.com>
2026-07-09 10:51:10 +0530

    Time: 4 ms (99.45%), Space: 114.2 MB (10.44%) - LeetHub

--- 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.java
+++ 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.java
@@ -6,7 +6,7 @@ class Solution {
         prime[1]=1;
         for(int i=2;i*i<n;i++){
             if(prime[i]==0){
-                for(int j=i*i;j<=n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     prime[j]=1;
                 }
             }
